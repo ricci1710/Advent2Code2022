1d0f400d9cdee3e554bbb4be6cf820113e6692da
Rico Kunz <rico.kunz@leutek.de>
2022-12-14 14:26:02 +0100

    day 14

---
+++ src/day13/logic13.js
@@ -0,0 +1,38 @@
+import {MOCK_DATA_DAY_13} from "./data13";
+import {MOCK_DEMO_DATA_DAY_13} from "./demo13";
+
+const Logic13 = () => {
+  // region prepare mock data
+  const data = MOCK_DATA_DAY_13.split('\n');
+  const demoData = MOCK_DEMO_DATA_DAY_13.split('\n');
+  // endregion prepare mock data
+  // region score rules
+  // endregion score rules
+  // region score calculation
+  const calcPartOne = () => {
+
+  };
+
+  const calcPartTwo = () => {
+
+  };
+  // endregion score calculation
+  // region print out part one
+  const demoScore = calcPartOne();
+  console.assert(demoScore === 21, `Algorithm is incorrect - expected: 21 calculated value: ${demoScore}`);
+  console.log('Demo-Score (Part One)  -> 21 ===', demoScore);
+
+  const lifeScore = calcPartOne();
+  console.log('Life-Score (Part One)  -> (???) 1700 ===', lifeScore);
+  // endregion print out part one
+  // region print out part two
+  const demoScorePT = calcPartTwo();
+  console.assert(demoScorePT === 21, `Algorithm is incorrect - expected: 21 calculated value: ${demoScorePT}`);
+  console.log('Demo-Score (Part Two)  -> 25 ===', demoScorePT);
+
+  const lifeScorePT = calcPartTwo();
+  console.log('Life-Score (Part Two)  -> (???) 2222 ===', lifeScorePT);
+  // endregion print out part two
+};
+
+export default Logic13;
