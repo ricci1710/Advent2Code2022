cef4a8926255a7e9210b66110235e0ae170f4c8e
ricci1710 <ricci1710@gmail.com>
2022-12-24 13:19:01 +0100

    Vorbereitung für Tag24

--- src/day24/Logic24.test.js
+++ src/day24/Logic24.test.js
@@ -1,4 +1,4 @@
-import {MOCK_DEMO_DATA_DAY_24} from "../day24/demo24";
+import {MOCK_DEMO_DATA_DAY_24} from "./demo24";
 import Day24 from "./Day24";
 import Array2d from "../utils/Array2d";
 
@@ -9,21 +9,21 @@ describe('Test Class Day24', () => {
 
     expect(day24).toBeDefined();
     const size = day24.playBoard.size();
-    expect(7).toEqual(size.x);
-    expect(7).toEqual(size.y);
+    expect(8).toEqual(size.x);
+    expect(6).toEqual(size.y);
   });
   test('getRowLine', () => {
     const demoData = MOCK_DEMO_DATA_DAY_24.split('\n');
     const day24 = new Day24(demoData);
     const rowLine = day24.playBoard.getRowLine(4);
-    expect(7).toEqual(rowLine.length);
-    expect('#...v.#').toEqual(Array2d.lineToString(rowLine));
+    expect(8).toEqual(rowLine.length);
+    expect('#<^v^^>#').toEqual(Array2d.lineToString(rowLine));
   });
   test('getColumnLine', () => {
     const demoData = MOCK_DEMO_DATA_DAY_24.split('\n');
     const day24 = new Day24(demoData);
     const columnLine = day24.playBoard.getColumnLine(4);
-    expect(7).toEqual(columnLine.length);
-    expect('#...v.#').toEqual(Array2d.lineToString(columnLine));
+    expect(6).toEqual(columnLine.length);
+    expect('#<.>^#').toEqual(Array2d.lineToString(columnLine));
   });
 });
